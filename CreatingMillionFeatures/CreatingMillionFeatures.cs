a86dcf4dbfba8daf2b7dac04e52816b925c35d26
hussein <hus.mhd@gmail.com>
2017-03-26 15:59:36 -0700

    million features

--- CreatingMillionFeatures/CreatingMillionFeatures.cs
+++ CreatingMillionFeatures/CreatingMillionFeatures.cs
@@ -18,55 +18,39 @@ namespace CreatingMillionFeatures
 
         protected override void OnClick()
         {
-            //
-            //  TODO: Sample code showing how to access button host
-            //
-            DateTime current = DateTime.Now;
-            long features = 10000;
-            ArcMap.Application.CurrentTool = null;
 
-            IMxDocument pMxDoc = ArcMap.Application.Document as IMxDocument;
-            IFeatureLayer pFLayer = pMxDoc.FocusMap.Layer[0] as IFeatureLayer;
-            IDataset pDS = pFLayer.FeatureClass as IDataset;
+            int features = 5000;
+            IMxDocument pmxdoc = ArcMap.Application.Document as IMxDocument;
+            IFeatureLayer pflayer = pmxdoc.FocusMap.Layer[0] as IFeatureLayer;
+            IDataset pDs = pflayer.FeatureClass as IDataset;
+            IWorkspaceEdit pWorkspace = pDs.Workspace as IWorkspaceEdit;
+            pWorkspace.StartEditing(false);
+            pWorkspace.StartEditOperation();
 
-            IWorkspaceEdit pWorkspaceEdit = pDS.Workspace as IWorkspaceEdit;
-            pWorkspaceEdit.StartEditing(false);
-            pWorkspaceEdit.StartEditOperation();
 
-            IFeatureBuffer pFeatureBuffer = pFLayer.FeatureClass.CreateFeatureBuffer();
-            IFeatureCursor pFCursor = pFLayer.FeatureClass.Insert(true);
-
-            IPoint pPoint;
+            IFeatureBuffer pBuffer = pflayer.FeatureClass.CreateFeatureBuffer();
+            IFeatureCursor pFcursor = pflayer.FeatureClass.Insert(true);
             Random r = new Random();
-            for (int i = 0; i < features; i++)
+            for (int i =0; i < features; i ++)
             {
-                //create a random point after each 
-                pPoint = new Point();
-                //-11,223,459.260  6,890,973.469 Meters
-                
-               
-                pPoint.X = -11223459.260 + r.NextDouble() * 2000; 
-                pPoint.Y = 6890973.469 + r.NextDouble() * 2000;
+                //-12,232,313.174  7,222,833.142
+                IPoint pPoint = new Point();
+                pPoint.X = -12232313.174 + r.NextDouble() * 2000;
+                pPoint.Y = 7222833.142 + r.NextDouble() * 2000;
 
-                pFeatureBuffer.Shape = pPoint;
-                pFeatureBuffer.Value[pFeatureBuffer.Fields.FindField("NAME")] = i.ToString();
-
-                pFCursor.InsertFeature(pFeatureBuffer);
-                //flush to geodatabase after each 1000 features
+                pBuffer.Shape = pPoint;
+                pBuffer.Value[pBuffer.Fields.FindField("NAME")] = "myfeature" + i.ToString();
+                pFcursor.InsertFeature(pBuffer);
                 if (i % 1000 == 0)
-                    pFCursor.Flush();
+                    pFcursor.Flush();
             }
-            //final flush
-            pFCursor.Flush();
-            pWorkspaceEdit.StopEditOperation();
-            //save edits
-            pWorkspaceEdit.StopEditing(true);
 
-            double elapsed = (DateTime.Now - current).TotalMilliseconds;
-            MessageBox.Show("Created " + features + " features in " + elapsed / 1000 + " seconds");
+            pFcursor.Flush();
+            pWorkspace.StopEditOperation();
+            pWorkspace.StopEditing(true);
 
-            pMxDoc.ActiveView.Refresh();
-             
+            pmxdoc.ActiveView.Refresh();
+
 
         }
         protected override void OnUpdate()
