653c11663f73090b7a317f320e0b5fd4d97b6313
hussein <hus.mhd@gmail.com>
2017-04-09 14:01:37 -0700

    working with domains

---
+++ WorkingWithCodedValueDomains/WorkingWithCodedValueDomains.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesGDB;
+using System.Windows.Forms;
+
+namespace WorkingWithCodedValueDomains
+{
+    public class WorkingWithCodedValueDomains : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public WorkingWithCodedValueDomains()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            IWorkspaceFactory pWorkspaceFactory = new FileGDBWorkspaceFactory();
+            IWorkspace pWorkspace = pWorkspaceFactory.OpenFromFile(@"C:\IGeometry\IGeometryGDB\IGeometry.gdb", ArcMap.Application.hWnd);
+
+            IWorkspaceDomains pWorkspaceDomains = pWorkspace as IWorkspaceDomains;
+            ICodedValueDomain pDomain = pWorkspaceDomains.DomainByName["RestaurantTypes"] as ICodedValueDomain;
+
+            for (int i = 0; i < pDomain.CodeCount; i ++)
+            {
+                MessageBox.Show("Code: " + pDomain.Value[i] + Environment.NewLine + "Description: " + pDomain.Name[i]);
+
+            }
+                
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
