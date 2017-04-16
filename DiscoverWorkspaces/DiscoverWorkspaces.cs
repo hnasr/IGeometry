82b85f0108bdd08f46100a80a0fdb5f969539796
hussein <hus.mhd@gmail.com>
2017-04-16 12:52:45 -0700

    discover workspaces

---
+++ DiscoverWorkspaces/DiscoverWorkspaces.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+using System.Windows.Forms;
+using ESRI.ArcGIS.esriSystem;
+
+namespace DiscoverWorkspaces
+{
+    public class DiscoverWorkspaces : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+
+        private Dictionary<string, IWorkspace> _workspaces = new Dictionary<string, IWorkspace>();
+
+        public void AddWorkspace(IWorkspace pWorkspace)
+        {
+
+            foreach (KeyValuePair<string, IWorkspace> pair in _workspaces)
+            {
+                if (pair.Value.Equals(pWorkspace))
+                    return;
+            }
+             
+            _workspaces.Add(Guid.NewGuid().ToString(), pWorkspace);
+
+        }
+
+        public void DiscoverWorkspacesMethod()
+        {
+
+            IMxDocument pMxdoc = ArcMap.Application.Document as IMxDocument;
+           IEnumLayer eLayers = pMxdoc.FocusMap.Layers;
+            eLayers.Reset();
+            ILayer pLayer = eLayers.Next();
+
+            while (pLayer != null)
+            {
+
+                if (pLayer is IFeatureLayer)
+                {
+                    IFeatureLayer pFLayer = pLayer as IFeatureLayer;
+                    IDataset ds = pFLayer.FeatureClass as IDataset;
+                    AddWorkspace(ds.Workspace);
+                }
+
+                pLayer = eLayers.Next();
+            }
+
+        }
+
+        public void ListWorkspaces()
+        {
+
+            foreach (KeyValuePair<string, IWorkspace> pair in _workspaces)
+            {
+
+                IPropertySet ps = pair.Value.ConnectionProperties;
+                string workspacestring = ps.GetProperty("DATABASE").ToString();
+                workspacestring = workspacestring + Environment.NewLine;
+                if (pair.Value.Type == esriWorkspaceType.esriRemoteDatabaseWorkspace)
+                {
+
+                    workspacestring = workspacestring +Environment.NewLine + ps.GetProperty("USER").ToString();
+                    workspacestring = workspacestring + Environment.NewLine + ps.GetProperty("VERSION").ToString();
+
+                }
+                MessageBox.Show(pair.Key + ":" + workspacestring);
+                 
+            }
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+
+            DiscoverWorkspacesMethod();
+
+            Form1 f = new Form1();
+            f.Workspaces = _workspaces;
+            foreach(string k in _workspaces.Keys)
+            {
+                f.addWorkspace(k);
+            }
+            f.ShowDialog();
+             
+          //  ListWorkspaces();
+        }
+
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
---
+++ DiscoverWorkspaces/Form1.cs
@@ -0,0 +1,54 @@
+﻿using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscoverWorkspaces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public Dictionary<string, IWorkspace> Workspaces = new Dictionary<string, IWorkspace>();
+
+        public void addWorkspace(string key)
+        {
+            comboBox1.Items.Add(key);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string g = comboBox1.SelectedItem.ToString();
+          IWorkspace pWorkspace = Workspaces[g];
+
+            IPropertySet ps = pWorkspace.ConnectionProperties;
+            string workspacestring = ps.GetProperty("DATABASE").ToString();
+            workspacestring = workspacestring + Environment.NewLine;
+            if (pWorkspace.Type == esriWorkspaceType.esriRemoteDatabaseWorkspace)
+            {
+
+                workspacestring = workspacestring + Environment.NewLine + ps.GetProperty("USER").ToString();
+                workspacestring = workspacestring + Environment.NewLine + ps.GetProperty("VERSION").ToString();
+
+            }
+
+            textBox1.Text = workspacestring;
+
+        }
+    }
+}
