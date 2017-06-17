69102c59a91d1a34cebef7234d6b83d71cfb76fa
hussein <hus.mhd@gmail.com>
2017-06-17 11:05:31 -0700

    find layer project

---
+++ EditingFeatureAttributes/EditingFeatureAttributes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace EditingFeatureAttributes
+{
+    public class EditingFeatureAttributes : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public EditingFeatureAttributes()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+            IMxDocument pMxDoc = ArcMap.Application.Document as IMxDocument;
+            IFeatureLayer pFLayer = pMxDoc.FocusMap.Layer[0] as IFeatureLayer;
+
+            IFeature pFeature = pFLayer.FeatureClass.GetFeature(1);
+            IWorkspaceEdit pWSE = ((IDataset)pFLayer.FeatureClass).Workspace as IWorkspaceEdit;
+            pWSE.StartEditing(false);
+            pWSE.StartEditOperation();
+
+            pFeature.Value[pFeature.Class.FindField("CONNECTEDTOWERID")] = "My New Tower ID";
+            pFeature.Store();
+
+            pWSE.StopEditOperation();
+            pWSE.StopEditing(true);
+                 
+             
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
---
+++ FindLayer/FindLayer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using System.Windows.Forms;
+
+namespace FindLayer
+{
+    public class FindLayer : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public FindLayer()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+
+            IMxDocument pMxDoc = ArcMap.Application.Document as IMxDocument;
+            IEnumLayer eLayers = pMxDoc.FocusMap.Layers;
+            eLayers.Reset();
+            ILayer pLayer = eLayers.Next();
+            while (pLayer != null)
+            {
+               
+                if (pLayer is IFeatureLayer)
+                {
+
+                    IFeatureLayer pFLayer = (IFeatureLayer)pLayer;
+                   if (pFLayer.FeatureClass.AliasName == "Device")
+                    {
+                        MessageBox.Show(pFLayer.FeatureClass.FeatureCount(null).ToString());
+
+                        return;
+                    }
+
+                }
+
+                pLayer = eLayers.Next();
+            }
+
+
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
