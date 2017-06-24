6a3ec45183e8c56d76999642cd99c3777761c4b8
hussein <hus.mhd@gmail.com>
2017-06-23 21:40:46 -0700

    installing new graphics 2

--- WorkingWithGraphics/CreatePolyline.cs
+++ WorkingWithGraphics/CreatePolyline.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using ESRI.ArcGIS.Desktop.AddIns;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Carto;
 
 namespace WorkingWithGraphics
 {
@@ -15,6 +20,32 @@ namespace WorkingWithGraphics
         {
 
         }
+
+        int i = 0;
+        protected override void OnMouseDown(MouseEventArgs arg)
+        {
+
+
+            IMxDocument pmxdoc = ArcMap.Application.Document as IMxDocument;
+            IRubberBand pRubber = new RubberLine();
+            IPolyline pPolyline = pRubber.TrackNew(pmxdoc.ActiveView.ScreenDisplay, null) as IPolyline;
+
+            IElement pElement = new LineElement();
+            ILineElement pLineElement = pElement as ILineElement;
+            pLineElement.Symbol = new SimpleLineSymbol();
+            pElement.Geometry = pPolyline;
+
+            IElementProperties pElementProp = pElement as IElementProperties;
+            pElementProp.Name = "Hussein Element " + ++i;
+
+            IGraphicsContainer pgc = pmxdoc.ActiveView.GraphicsContainer;
+            pgc.AddElement(pElement, 0);
+            pmxdoc.ActiveView.Refresh();
+                
+
+        }
+
+
     }
 
 }
---
+++ WorkingWithGraphics/IdentifyElement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Desktop.AddIns;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Carto;
+using System.Windows.Forms;
+
+namespace WorkingWithGraphics
+{
+    public class IdentifyElement : ESRI.ArcGIS.Desktop.AddIns.Tool
+    {
+        public IdentifyElement()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+
+        }
+
+        protected override void OnMouseDown(MouseEventArgs arg)
+        {
+            IMxDocument pmxdoc = ArcMap.Application.Document as IMxDocument;
+
+            IPoint pPoint = pmxdoc.ActiveView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y);
+
+            IGraphicsContainer pgc = pmxdoc.ActiveView.GraphicsContainer;
+            IEnumElement eElements = pgc.LocateElements(pPoint, 5);
+            if (eElements == null) return;
+
+            eElements.Reset();
+            IElement pElement = eElements.Next();
+            while(pElement != null)
+            {
+                IElementProperties pElementProps = pElement as IElementProperties;
+                
+                MessageBox.Show(pElementProps.Name);
+
+                pElement = eElements.Next();
+            }
+        }
+    }
+
+}
